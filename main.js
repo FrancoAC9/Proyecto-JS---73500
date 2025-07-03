670eda01c3d85aa685be265316efcbaf818e9b9f
Franco <francocanova10@gmail.com>
2025-07-03 19:32:02 -0300

    Práctica JS

---
+++ main.js
@@ -0,0 +1,44 @@
+prompt("ingresa un numero");
+alert("numero incorrecto");
+
+let emaildelusuario = "jorgeluis@gmail.com";
+let nombre = "Jorge";
+let password = "cumbia";
+let edad = "35";
+
+console.log(nombre);
+console.log(password);
+console.log(emaildelusuario);
+console.log(edad);
+
+edad = 37;
+console.log(edad);
+
+let numero = 12;
+console.log(numero);
+
+let Nombre_prompt = prompt("ingresa un numero");
+console.log(Nombre_prompt);
+
+let numeroincorrecto = true;
+let ingresaunnumero = false;
+console.log(numeroincorrecto);
+console.log(ingresaunnumero);
+
+let saludo = "Hola como estas " + nombre + " todo bien?";
+console.log(saludo);
+
+let despedida = `Adios ${password} nos vemos pronto`;
+console.log(despedida);
+
+let X = 12;
+let Y = 12;
+console.log(X == Y);
+let A = 13;
+let B = 14;
+console.log(A == B);
+console.log(X != Y);
+console.log(X >= Y);
+console.log(X <= Y);
+console.log(X < Y);
+console.log(X > Y);
