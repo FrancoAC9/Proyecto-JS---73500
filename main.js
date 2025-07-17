3611e6e8b1f24cd4eb025bce915d95be9fc4190f
Franco <francocanova10@gmail.com>
2025-07-17 09:09:03 -0300

    modificación de prueba

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ alert("numero incorrecto");
 let emaildelusuario = "jorgeluis@gmail.com";
 let nombre = "Jorge";
 let password = "cumbia";
-let edad = "35";
+let edad = "60";
 
 console.log(nombre);
 console.log(password);
 console.log(emaildelusuario);
 console.log(edad);
 
-edad = 37;
+edad = 60;
 console.log(edad);
 
 let numero = 12;
@@ -42,3 +42,22 @@ console.log(X >= Y);
 console.log(X <= Y);
 console.log(X < Y);
 console.log(X > Y);
+
+let temperatura = prompt("ingresa la temperatura");
+if (temperatura < 20 && temperatura >= 10) {
+  console.log("otoño");
+} else if (temperatura >= 20 && temperatura < 30) {
+  console.log("primavera");
+} else if (temperatura >= 30 && temperatura < 40) {
+  console.log("verano");
+} else if (temperatura < 10 && temperatura >= 0) {
+  console.log("invierno");
+} else {
+  console.log("alerta de clima extremo");
+}
+
+console.log("Inicio de bucle");
+for (let i = 20; i < 35; i++) {
+  console.log("Ja Ja");
+}
+console.log("Fin de bucle");
